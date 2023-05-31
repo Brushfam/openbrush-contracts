a1676048fe676198beccef997f7b33145947b1ec
Dominik | Supercolony <43150707+coreggon11@users.noreply.github.com>
2023-05-31 14:54:26 +0200

    impl new trait system in psp22 mintable example

--- examples/psp22_extensions/mintable/lib.rs
+++ examples/psp22_extensions/mintable/lib.rs
@@ -14,23 +14,142 @@ pub mod my_psp22_mintable {
         psp22: psp22::Data,
     }
 
-    impl PSP22 for Contract {}
+    impl psp22::InternalImpl for Contract {}
 
-    impl PSP22Mintable for Contract {}
+    impl psp22::Internal for Contract {
+        fn _emit_transfer_event(&self, from: Option<AccountId>, to: Option<AccountId>, amount: Balance) {
+            psp22::InternalImpl::_emit_transfer_event(self, from, to, amount)
+        }
+
+        fn _emit_approval_event(&self, owner: AccountId, spender: AccountId, amount: Balance) {
+            psp22::InternalImpl::_emit_approval_event(self, owner, spender, amount)
+        }
+
+        fn _total_supply(&self) -> Balance {
+            psp22::InternalImpl::_total_supply(self)
+        }
+
+        fn _balance_of(&self, owner: &AccountId) -> Balance {
+            psp22::InternalImpl::_balance_of(self, owner)
+        }
+
+        fn _allowance(&self, owner: &AccountId, spender: &AccountId) -> Balance {
+            psp22::InternalImpl::_allowance(self, owner, spender)
+        }
+
+        fn _transfer_from_to(
+            &mut self,
+            from: AccountId,
+            to: AccountId,
+            amount: Balance,
+            data: Vec<u8>,
+        ) -> Result<(), PSP22Error> {
+            psp22::InternalImpl::_transfer_from_to(self, from, to, amount, data)
+        }
+
+        fn _approve_from_to(
+            &mut self,
+            owner: AccountId,
+            spender: AccountId,
+            amount: Balance,
+        ) -> Result<(), PSP22Error> {
+            psp22::InternalImpl::_approve_from_to(self, owner, spender, amount)
+        }
+
+        fn _mint_to(&mut self, account: AccountId, amount: Balance) -> Result<(), PSP22Error> {
+            psp22::InternalImpl::_mint_to(self, account, amount)
+        }
+
+        fn _burn_from(&mut self, account: AccountId, amount: Balance) -> Result<(), PSP22Error> {
+            psp22::InternalImpl::_burn_from(self, account, amount)
+        }
+
+        fn _before_token_transfer(
+            &mut self,
+            from: Option<&AccountId>,
+            to: Option<&AccountId>,
+            amount: &Balance,
+        ) -> Result<(), PSP22Error> {
+            psp22::InternalImpl::_before_token_transfer(self, from, to, amount)
+        }
+
+        fn _after_token_transfer(
+            &mut self,
+            from: Option<&AccountId>,
+            to: Option<&AccountId>,
+            amount: &Balance,
+        ) -> Result<(), PSP22Error> {
+            psp22::InternalImpl::_after_token_transfer(self, from, to, amount)
+        }
+    }
+
+    impl PSP22Impl for Contract {}
+
+    impl PSP22 for Contract {
+        #[ink(message)]
+        fn total_supply(&self) -> Balance {
+            PSP22Impl::total_supply(self)
+        }
+
+        #[ink(message)]
+        fn balance_of(&self, owner: AccountId) -> Balance {
+            PSP22Impl::balance_of(self, owner)
+        }
+
+        #[ink(message)]
+        fn allowance(&self, owner: AccountId, spender: AccountId) -> Balance {
+            PSP22Impl::allowance(self, owner, spender)
+        }
+
+        #[ink(message)]
+        fn transfer(&mut self, to: AccountId, value: Balance, data: Vec<u8>) -> Result<(), PSP22Error> {
+            PSP22Impl::transfer(self, to, value, data)
+        }
+
+        #[ink(message)]
+        fn transfer_from(
+            &mut self,
+            from: AccountId,
+            to: AccountId,
+            value: Balance,
+            data: Vec<u8>,
+        ) -> Result<(), PSP22Error> {
+            PSP22Impl::transfer_from(self, from, to, value, data)
+        }
+
+        #[ink(message)]
+        fn approve(&mut self, spender: AccountId, value: Balance) -> Result<(), PSP22Error> {
+            PSP22Impl::approve(self, spender, value)
+        }
+
+        #[ink(message)]
+        fn increase_allowance(&mut self, spender: AccountId, delta_value: Balance) -> Result<(), PSP22Error> {
+            PSP22Impl::increase_allowance(self, spender, delta_value)
+        }
+
+        #[ink(message)]
+        fn decrease_allowance(&mut self, spender: AccountId, delta_value: Balance) -> Result<(), PSP22Error> {
+            PSP22Impl::decrease_allowance(self, spender, delta_value)
+        }
+    }
+
+    impl PSP22MintableImpl for Contract {}
+
+    impl PSP22Mintable for Contract {
+        #[ink(message)]
+        fn mint(&mut self, account: AccountId, amount: Balance) -> Result<(), PSP22Error> {
+            PSP22MintableImpl::mint(self, account, amount)
+        }
+    }
 
     impl Contract {
         #[ink(constructor)]
         pub fn new(total_supply: Balance) -> Self {
             let mut instance = Self::default();
 
-            assert!(instance._mint_to(Self::env().caller(), total_supply).is_ok());
+            psp22::Internal::_mint_to(&mut instance, Self::env().caller(), total_supply).expect("Should mint");
 
             instance
-        }
-
-        #[ink(message)]
-        pub fn mint_to(&mut self, account: AccountId, amount: Balance) -> Result<(), PSP22Error> {
-            self.mint(account, amount)
         }
     }
 

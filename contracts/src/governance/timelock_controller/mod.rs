bfa4c61874ee4bcd12c1b882c7c0f80ad4bf316e
Dominik | Supercolony <43150707+coreggon11@users.noreply.github.com>
2023-06-05 22:19:43 +0200

    impl new trait system in timelock controller

--- contracts/src/governance/timelock_controller/mod.rs
+++ contracts/src/governance/timelock_controller/mod.rs
@@ -27,9 +27,11 @@ pub use crate::{
         timelock_controller::*,
     },
 };
-pub use access_control::Internal as _;
-pub use timelock_controller::Internal as _;
-
+pub use access_control::{
+    AccessControlImpl,
+    Internal as _,
+    InternalImpl as _,
+};
 use core::convert::TryFrom;
 use ink::{
     env::{
@@ -46,12 +48,6 @@ use ink::{
         vec,
         vec::Vec,
     },
-    storage::traits::{
-        AutoStorableHint,
-        ManualKey,
-        Storable,
-        StorableHint,
-    },
 };
 use openbrush::{
     modifier_definition,
@@ -60,12 +56,12 @@ use openbrush::{
     traits::{
         AccountId,
         Hash,
-        OccupiedStorage,
         Storage,
         Timestamp,
         ZERO_ADDRESS,
     },
 };
+pub use timelock_controller::Internal as _;
 
 pub const STORAGE_KEY: u32 = openbrush::storage_unique_key!(Data);
 
@@ -82,19 +78,14 @@ pub struct Data {
 /// considered. Granting a role to zero account is equivalent to enabling
 /// this role for everyone.
 #[modifier_definition]
-pub fn only_role_or_open_role<T, M, F, R, E>(instance: &mut T, body: F, role: RoleType) -> Result<R, E>
+pub fn only_role_or_open_role<T, F, R, E>(instance: &mut T, body: F, role: RoleType) -> Result<R, E>
 where
-    M: access_control::members::MembersManager,
-    M: Storable
-        + StorableHint<ManualKey<{ access_control::STORAGE_KEY }>>
-        + AutoStorableHint<ManualKey<3218979580, ManualKey<{ access_control::STORAGE_KEY }>>, Type = M>,
-    T: Storage<access_control::Data<M>>,
-    T: OccupiedStorage<{ access_control::STORAGE_KEY }, WithData = access_control::Data<M>>,
+    T: access_control::Internal + access_control::MembersManager + Storage<access_control::Data>,
     F: FnOnce(&mut T) -> Result<R, E>,
     E: From<AccessControlError>,
 {
-    if !instance.data().members.has_role(role, &(ZERO_ADDRESS.into())) {
-        access_control::check_role(instance, role, T::env().caller())?;
+    if !instance._has_role(role, &(ZERO_ADDRESS.into())) {
+        instance._check_role(role, T::env().caller())?;
     }
     body(instance)
 }
@@ -105,47 +96,34 @@ pub const EXECUTOR_ROLE: RoleType = ink::selector_id!("EXECUTOR_ROLE");
 
 pub const DONE_TIMESTAMP: Timestamp = 1;
 
-impl<T, M> TimelockController for T
-where
-    M: access_control::members::MembersManager,
-    M: Storable
-        + StorableHint<ManualKey<{ access_control::STORAGE_KEY }>>
-        + AutoStorableHint<ManualKey<3218979580, ManualKey<{ access_control::STORAGE_KEY }>>, Type = M>,
-    T: Storage<Data>,
-    T: Storage<access_control::Data<M>>,
-    T: OccupiedStorage<{ access_control::STORAGE_KEY }, WithData = access_control::Data<M>>,
+pub trait TimelockControllerImpl:
+    Internal + Storage<Data> + access_control::MembersManager + access_control::Internal + Storage<access_control::Data>
 {
     fn is_operation(&self, id: OperationId) -> bool {
-        self.get_timestamp(id) > Timestamp::default()
+        self._is_operation(id)
     }
 
     fn is_operation_pending(&self, id: OperationId) -> bool {
-        self.get_timestamp(id) > Self::_done_timestamp()
+        self._get_timestamp(id) > Self::_done_timestamp()
     }
 
     fn is_operation_ready(&self, id: OperationId) -> bool {
-        let timestamp = self.get_timestamp(id);
-        timestamp > Self::_done_timestamp() && timestamp <= Self::env().block_timestamp()
+        self._is_operation_ready(id)
     }
 
     fn is_operation_done(&self, id: OperationId) -> bool {
-        self.get_timestamp(id) == Self::_done_timestamp()
+        self._is_operation_done(id)
     }
 
     fn get_timestamp(&self, id: OperationId) -> Timestamp {
-        self.data::<Data>().timestamps.get(&id).unwrap_or(Timestamp::default())
+        self._get_timestamp(id)
     }
 
     fn get_min_delay(&self) -> Timestamp {
         self.data::<Data>().min_delay.clone()
     }
 
-    fn hash_operation(
-        &self,
-        transaction: Transaction,
-        predecessor: Option<OperationId>,
-        salt: [u8; 32],
-    ) -> Hash {
+    fn hash_operation(&self, transaction: Transaction, predecessor: Option<OperationId>, salt: [u8; 32]) -> Hash {
         self._hash_operation(&transaction, &predecessor, &salt)
     }
 
@@ -158,7 +136,7 @@ where
         self._hash_operation_batch(&transactions, &predecessor, &salt)
     }
 
-    #[modifiers(access_control::only_role(Self::_proposal_role()))]
+    #[modifiers(access_control::only_role(<Self as Internal>::_proposal_role()))]
     fn schedule(
         &mut self,
         transaction: Transaction,
@@ -174,7 +152,7 @@ where
         Ok(())
     }
 
-    #[modifiers(access_control::only_role(Self::_proposal_role()))]
+    #[modifiers(access_control::only_role(<Self as Internal>::_proposal_role()))]
     fn schedule_batch(
         &mut self,
         transactions: Vec<Transaction>,
@@ -192,7 +170,7 @@ where
         Ok(())
     }
 
-    #[modifiers(access_control::only_role(Self::_proposal_role()))]
+    #[modifiers(access_control::only_role(<Self as Internal>::_proposal_role()))]
     fn cancel(&mut self, id: OperationId) -> Result<(), TimelockControllerError> {
         if !self.is_operation_pending(id) {
             return Err(TimelockControllerError::OperationCannonBeCanceled)
@@ -203,7 +181,7 @@ where
         Ok(())
     }
 
-    #[modifiers(only_role_or_open_role(Self::_executor_role()))]
+    #[modifiers(only_role_or_open_role(<Self as Internal>::_executor_role()))]
     fn execute(
         &mut self,
         transaction: Transaction,
@@ -217,7 +195,7 @@ where
         self._after_call(id)
     }
 
-    #[modifiers(only_role_or_open_role(Self::_executor_role()))]
+    #[modifiers(only_role_or_open_role(<Self as Internal>::_executor_role()))]
     fn execute_batch(
         &mut self,
         transactions: Vec<Transaction>,
@@ -249,17 +227,20 @@ where
 
 pub trait Internal {
     /// User must override those methods in their contract.
-    fn _emit_min_delay_change_event(&self, _old_delay: Timestamp, _new_delay: Timestamp);
+    fn _emit_min_delay_change_event(&self, old_delay: Timestamp, new_delay: Timestamp);
+
     fn _emit_call_scheduled_event(
         &self,
-        _id: OperationId,
-        _index: u8,
-        _transaction: Transaction,
-        _predecessor: Option<OperationId>,
-        _delay: Timestamp,
+        id: OperationId,
+        index: u8,
+        transaction: Transaction,
+        predecessor: Option<OperationId>,
+        delay: Timestamp,
     );
-    fn _emit_cancelled_event(&self, _id: OperationId);
-    fn _emit_call_executed_event(&self, _id: OperationId, _index: u8, _transaction: Transaction);
+
+    fn _emit_cancelled_event(&self, id: OperationId);
+
+    fn _emit_call_executed_event(&self, id: OperationId, index: u8, transaction: Transaction);
 
     fn _init_with_caller(&mut self, min_delay: Timestamp, proposers: Vec<AccountId>, executors: Vec<AccountId>);
 
@@ -285,7 +266,7 @@ pub trait Internal {
         salt: &[u8; 32],
     ) -> OperationId;
 
-    /// Schedule an operation that is to becomes valid after a given delay.
+    /// Schedule an operation that is to become valid after a given delay.
     fn _schedule(&mut self, id: OperationId, delay: &Timestamp) -> Result<(), TimelockControllerError>;
 
     /// Checks before execution of an operation's calls.
@@ -297,7 +278,7 @@ pub trait Internal {
     /// Execute an operation's call.
     ///
     /// Emits a `CallExecuted` event.
-    fn _call(&mut self, id: OperationId, i: u8, transaction: Transaction) -> Result<(), TimelockControllerError>;
+    fn _call(&mut self, id: OperationId, index: u8, transaction: Transaction) -> Result<(), TimelockControllerError>;
 
     fn _timelock_admin_role() -> RoleType;
 
@@ -306,19 +287,19 @@ pub trait Internal {
     fn _executor_role() -> RoleType;
 
     fn _done_timestamp() -> Timestamp;
+
+    fn _is_operation(&self, id: OperationId) -> bool;
+
+    fn _is_operation_ready(&self, id: OperationId) -> bool;
+
+    fn _is_operation_done(&self, id: OperationId) -> bool;
+
+    fn _get_timestamp(&self, id: OperationId) -> Timestamp;
 }
 
-impl<T, M> Internal for T
-where
-    M: access_control::members::MembersManager,
-    M: Storable
-        + StorableHint<ManualKey<{ access_control::STORAGE_KEY }>>
-        + AutoStorableHint<ManualKey<3218979580, ManualKey<{ access_control::STORAGE_KEY }>>, Type = M>,
-    T: Storage<Data>,
-    T: Storage<access_control::Data<M>>,
-    T: OccupiedStorage<{ access_control::STORAGE_KEY }, WithData = access_control::Data<M>>,
-{
+pub trait InternalImpl: Internal + Storage<Data> + access_control::Internal {
     fn _emit_min_delay_change_event(&self, _old_delay: Timestamp, _new_delay: Timestamp) {}
+
     fn _emit_call_scheduled_event(
         &self,
         _id: OperationId,
@@ -328,15 +309,12 @@ where
         _delay: Timestamp,
     ) {
     }
+
     fn _emit_cancelled_event(&self, _id: OperationId) {}
+
     fn _emit_call_executed_event(&self, _id: OperationId, _index: u8, _transaction: Transaction) {}
 
-    fn _init_with_caller(
-        &mut self,
-        min_delay: Timestamp,
-        proposers: Vec<AccountId>,
-        executors: Vec<AccountId>,
-    ) {
+    fn _init_with_caller(&mut self, min_delay: Timestamp, proposers: Vec<AccountId>, executors: Vec<AccountId>) {
         let caller = Self::env().caller();
         Internal::_init_with_admin(self, caller, min_delay, proposers, executors);
     }
@@ -348,26 +326,35 @@ where
         proposers: Vec<AccountId>,
         executors: Vec<AccountId>,
     ) {
-        self._set_role_admin(Self::_timelock_admin_role(), Self::_timelock_admin_role());
-        self._set_role_admin(Self::_proposal_role(), Self::_proposal_role());
-        self._set_role_admin(Self::_executor_role(), Self::_executor_role());
+        self._set_role_admin(
+            <Self as Internal>::_timelock_admin_role(),
+            <Self as Internal>::_timelock_admin_role(),
+        );
+        self._set_role_admin(
+            <Self as Internal>::_proposal_role(),
+            <Self as Internal>::_proposal_role(),
+        );
+        self._set_role_admin(
+            <Self as Internal>::_executor_role(),
+            <Self as Internal>::_executor_role(),
+        );
 
         // admin + self administration
-        self._setup_role(Self::_timelock_admin_role(), Self::env().account_id());
-        self._setup_role(Self::_timelock_admin_role(), admin);
+        self._setup_role(<Self as Internal>::_timelock_admin_role(), Self::env().account_id());
+        self._setup_role(<Self as Internal>::_timelock_admin_role(), admin);
 
         // register proposers
         proposers
             .into_iter()
-            .for_each(|proposer| self._setup_role(Self::_proposal_role(), proposer));
+            .for_each(|proposer| self._setup_role(<Self as Internal>::_proposal_role(), proposer));
         // register executors
         executors
             .into_iter()
-            .for_each(|executor| self._setup_role(Self::_executor_role(), executor));
+            .for_each(|executor| self._setup_role(<Self as Internal>::_executor_role(), executor));
 
         let old_delay = self.data::<Data>().min_delay.clone();
         self.data::<Data>().min_delay = min_delay;
-        self._emit_min_delay_change_event(old_delay, min_delay);
+        Internal::_emit_min_delay_change_event(self, old_delay, min_delay);
     }
 
     fn _hash_operation(
@@ -405,7 +392,7 @@ where
     }
 
     fn _schedule(&mut self, id: OperationId, delay: &Timestamp) -> Result<(), TimelockControllerError> {
-        if self.is_operation(id) {
+        if Internal::_is_operation(self, id) {
             return Err(TimelockControllerError::OperationAlreadyScheduled)
         }
         if delay < &self.data::<Data>().min_delay {
@@ -419,27 +406,24 @@ where
     }
 
     fn _before_call(&self, predecessor: Option<OperationId>) -> Result<(), TimelockControllerError> {
-        if predecessor.is_some() && !self.is_operation_done(predecessor.unwrap()) {
+        if predecessor.is_some() && !Internal::_is_operation_done(self, predecessor.unwrap()) {
             return Err(TimelockControllerError::MissingDependency)
         }
         Ok(())
     }
 
     fn _after_call(&mut self, id: OperationId) -> Result<(), TimelockControllerError> {
-        if !self.is_operation_ready(id) {
+        if !Internal::_is_operation_ready(self, id) {
             return Err(TimelockControllerError::OperationIsNotReady)
         }
 
-        self.data::<Data>().timestamps.insert(&id, &Self::_done_timestamp());
+        self.data::<Data>()
+            .timestamps
+            .insert(&id, &<Self as Internal>::_done_timestamp());
         Ok(())
     }
 
-    fn _call(
-        &mut self,
-        id: OperationId,
-        i: u8,
-        transaction: Transaction,
-    ) -> Result<(), TimelockControllerError> {
+    fn _call(&mut self, id: OperationId, i: u8, transaction: Transaction) -> Result<(), TimelockControllerError> {
         // Flush the state into storage before the cross call.
         // Because during cross call we cann call this contract(for example for `update_delay` method).
         self.flush();
@@ -459,7 +443,7 @@ where
         self.load();
 
         result?.unwrap();
-        self._emit_call_executed_event(id, i, transaction);
+        Internal::_emit_call_executed_event(self, id, i, transaction);
         Ok(())
     }
 
@@ -477,6 +461,23 @@ where
 
     fn _done_timestamp() -> Timestamp {
         DONE_TIMESTAMP
+    }
+
+    fn _is_operation(&self, id: OperationId) -> bool {
+        Internal::_get_timestamp(self, id) > Timestamp::default()
+    }
+
+    fn _is_operation_ready(&self, id: OperationId) -> bool {
+        let timestamp = Internal::_get_timestamp(self, id);
+        timestamp > <Self as Internal>::_done_timestamp() && timestamp <= Self::env().block_timestamp()
+    }
+
+    fn _is_operation_done(&self, id: OperationId) -> bool {
+        Internal::_get_timestamp(self, id) == <Self as Internal>::_done_timestamp()
+    }
+
+    fn _get_timestamp(&self, id: OperationId) -> Timestamp {
+        self.data::<Data>().timestamps.get(&id).unwrap_or(Timestamp::default())
     }
 }
 

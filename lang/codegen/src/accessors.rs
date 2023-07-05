3c4bdad21f2d07d16c4ade02eaee30ac563ea435
Dominik | Supercolony <43150707+coreggon11@users.noreply.github.com>
2023-07-05 14:56:19 +0200

    clippy VOL. 3

--- lang/codegen/src/accessors.rs
+++ lang/codegen/src/accessors.rs
@@ -78,7 +78,7 @@ fn generate_struct(s: &synstructure::Structure, struct_item: DataStruct) -> Toke
         .clone()
         .fields
         .into_iter()
-        .map(|field| consume_attrs(&mut field.clone()));
+        .map(|mut field| consume_attrs(&mut field));
 
     match struct_item.fields {
         Fields::Unnamed(_) => {

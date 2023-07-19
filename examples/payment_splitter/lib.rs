4e30b773bc4ab0433dfc8a786a1f841f228f9318
Varex Silver <limposfeed@gmail.com>
2023-07-19 23:38:35 +0300

    Fix CI

--- examples/payment_splitter/lib.rs
+++ examples/payment_splitter/lib.rs
@@ -3,7 +3,6 @@
 #[openbrush::implementation(PaymentSplitter)]
 #[openbrush::contract]
 pub mod my_payment_splitter {
-    use ink::prelude::vec::Vec;
     use openbrush::traits::Storage;
 
     #[ink(storage)]
@@ -39,11 +38,7 @@ pub mod my_payment_splitter {
         #[rustfmt::skip]
         use ink_e2e::{build_message, PolkadotConfig};
 
-        use test_helpers::{
-            address_of,
-            get_shares,
-            method_call,
-        };
+        use test_helpers::{address_of, get_shares, method_call};
 
         type E2EResult<T> = Result<T, Box<dyn std::error::Error>>;
 

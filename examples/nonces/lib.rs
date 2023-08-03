564cce60fb6a292270ed7921adc5d714d05cd3f6
prxgr4mm3r <berezan.ab@knu.ua>
2023-08-03 21:33:06 +0300

    Add nonces e2e tests

---
+++ examples/nonces/lib.rs
@@ -0,0 +1,55 @@
+#![cfg_attr(not(feature = "std"), no_std, no_main)]
+
+#[openbrush::contract]
+pub mod my_timelock_controller {
+    use ink::prelude::vec::Vec;
+    use openbrush::{
+        contracts::{
+            nonces,
+            nonces::{
+                NoncesError,
+                NoncesImpl,
+            },
+            traits::{
+                utils::nonces::*,
+            },
+        },
+        traits::{
+            Storage,
+            String,
+        },
+    };
+
+    #[ink(storage)]
+    #[derive(Default, Storage)]
+    pub struct Contract {
+        #[storage_field]
+        nonces: nonces::Data,
+    }
+
+    impl Contract {
+        #[ink(constructor)]
+        pub fn new() -> Self {
+            Self::default()
+        }
+
+        #[ink(message)]
+        pub fn use_nonce(&mut self, account: AccountId) -> Result<u128, NoncesError> {
+            NoncesImpl::_use_nonce(self, &account)
+        }
+
+        #[ink(message)]
+        pub fn use_checked_nonce(&mut self, account: AccountId, nonce: u128) -> Result<u128, NoncesError> {
+            NoncesImpl::_use_checked_nonce(self, &account, nonce)
+        }
+    }
+
+    impl NoncesImpl for Contract {}
+
+    impl Nonces for Contract {
+        #[ink(message)]
+        fn nonces(&self, account: AccountId) -> u128 {
+            NoncesImpl::nonces(self, &account)
+        }
+    }
+}
--- tests/e2e/governance/nonces.tests.ts
+++ tests/e2e/governance/nonces.tests.ts
@@ -1,33 +1,92 @@
+import { expect, getSigners } from '../helpers'
+import { ApiPromise } from '@polkadot/api'
+
+import Constructor from '../../../typechain-generated/constructors/nonces'
+import Contract from '../../../typechain-generated/contracts/nonces'
+import BN from "bn.js";
+import {Result} from "@727-ventures/typechain-types";
+
 describe('Nonces', function () {
+  async function setup() {
+    const api = await ApiPromise.create()
+
+    const signers = getSigners()
+    const deployer = signers[0]
+    const bob = signers[1]
+
+    const contractFactory = new Constructor(api, deployer)
+    const contractAddress = (await contractFactory.new()).address
+    const contract = new Contract(contractAddress, deployer, api)
+
+    return {
+      api,
+      bob,
+      deployer,
+      contract,
+    }
+  }
+
   beforeEach(async function () {
     //
   })
 
   it('gets a nonce', async function () {
-    //
+    const {
+      api,
+      bob,
+      deployer,
+      contract,
+    } = await setup()
+    const nonce = await contract.query.nonces(bob.address)
+    expect(await contract.query.nonces(bob.address)).to.have.output(new BN(0))
+    await api.disconnect()
   })
 
   describe('_useNonce', function () {
     it('increments a nonce', async function () {
-      //
+      const {
+        api,
+        bob,
+        deployer,
+        contract,
+      } = await setup()
+        expect(await contract.query.nonces(bob.address)).to.have.output(new BN(0))
+      expect(await contract.query.useNonce(bob.address)).to.have.output(new BN(0))
+      expect(await contract.query.useNonce(bob.address)).to.have.output(new BN(1))
+      expect(await contract.query.nonces(bob.address)).to.have.output(new BN(2))
+      await api.disconnect()
     })
 
-    it('increments only sender\'s nonce', async function () {
-      //
-    })
+    // it('increments only sender\'s nonce', async function () {
+    //   //
+    // })
   })
 
   describe('_useCheckedNonce', function () {
     it('increments a nonce', async function () {
-      //
+      const {
+        api,
+        bob,
+        deployer,
+        contract,
+      } = await setup()
+      expect(await contract.query.nonces(bob.address)).to.have.output(new BN(0))
+      expect(await contract.query.useCheckedNonce(bob.address, new BN(0))).to.have.output(new BN(0))
+      expect(await contract.query.useCheckedNonce(bob.address, new BN(1))).to.have.output(new BN(1))
     })
 
-    it('increments only sender\'s nonce', async function () {
-      //     
-    })
+    // it('increments only sender\'s nonce', async function () {
+    //   //
+    // })
 
     it('reverts when nonce is not the expected', async function () {
-      //     
+      const {
+        api,
+        bob,
+        deployer,
+        contract,
+      } = await setup()
+      //todo
     })
   })
 })

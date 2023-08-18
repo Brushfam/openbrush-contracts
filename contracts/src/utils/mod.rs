33ce3e9b8742c0dad3f81c5d40dc9dceb16b20ce
Artemka374 <fomiuk.artemka123@gmail.com>
2023-08-18 15:07:58 +0300

    add feature flags on utils

--- contracts/src/utils/mod.rs
+++ contracts/src/utils/mod.rs
@@ -20,6 +20,9 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+#[cfg(feature = "checkpoint")]
 pub mod checkpoint;
+#[cfg(feature = "crypto")]
 pub mod crypto;
+#[cfg(feature = "nonces")]
 pub mod nonces;

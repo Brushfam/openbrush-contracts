4249d3e009291deaa6a3f586e5b3c78f50ed3649
Artemka374 <fomiuk.artemka123@gmail.com>
2023-09-20 15:43:54 +0300

    fix build

--- examples/psp22/lib.rs
+++ examples/psp22/lib.rs
@@ -21,6 +21,7 @@ pub struct HatedStorage {
 #[openbrush::contract]
 pub mod my_psp22 {
     use crate::*;
+    use openbrush::traits::String;
 
     #[ink(storage)]
     #[derive(Storage)]
@@ -38,7 +39,7 @@ pub mod my_psp22 {
         to: Option<&AccountId>,
         _amount: &Balance,
     ) -> Result<(), PSP22Error> {
-        if to == Some(&self.hated_storage.hated_account) {
+        if _to == Some(&self.hated_storage.hated_account) {
             return Err(PSP22Error::Custom(String::from("I hate this account!")))
         }
         Ok(())
